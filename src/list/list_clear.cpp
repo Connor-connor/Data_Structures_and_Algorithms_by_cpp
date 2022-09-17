ad29e4c980f5755277e560d65d88a5f171b83ff9
lihaopeng <752869332@qq.com>
2022-09-17 20:41:17 +0800

    second commit

---
+++ src/list/list_clear.cpp
@@ -0,0 +1,8 @@
+#include "list.h"
+
+template <typename T> int List<T>::clear() { //清空列表
+    int oldSize = _size;
+    while ( 0 < _size ) remove ( header->succ ); //反复删除首节点，直至列表变空
+    return oldSize;
+}
+//TODO:会不会浪费空间资源?
